645ede169992af3503d5bf03d83d2821968098d3
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-21 16:39:10 +0200

    resource route

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -11,23 +11,25 @@
 
     <ul class="d-flex flex-wrap text-center justify-content-between">
        @foreach ($comics as $comic)
+       <a href="">
         <li>
             <div class="card p-2">
                 
-            
-                <div>
-                    <img class="img-fluid" src="{{ $comic->thumb }}" alt="">
+                
+                    <div>
+                        <img class="img-fluid" src="{{ $comic->thumb }}" alt="">
+                    </div>
+
+                    <div>
+                        {{ $comic->series }}
+                    </div>
+
+
+
                 </div>
 
-                <div>
-                    {{ $comic->series }}
-                </div>
-
-
-
-            </div>
-
-        </li> 
+            </li> 
+        </a>
         @endforeach
     </ul>
 
