e77878f75630122869a17717f786c1be53b7e661
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-21 16:23:20 +0200

    comics render ok

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -9,7 +9,27 @@
 <h1>Comics</h1>
 
 
-@dump($comics)
+    <ul class="d-flex flex-wrap text-center justify-content-between">
+       @foreach ($comics as $comic)
+        <li>
+            <div class="card p-2">
+                
+            
+                <div>
+                    <img class="img-fluid" src="{{ $comic->thumb }}" alt="">
+                </div>
+
+                <div>
+                    {{ $comic->series }}
+                </div>
+
+
+
+            </div>
+
+        </li> 
+        @endforeach
+    </ul>
 
 
 @endsection
