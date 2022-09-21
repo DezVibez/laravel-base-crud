af732c8cb8cf25cdbc0a637c49c09d044cc5f08d
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-21 14:21:57 +0200

    rotta index e rotta home

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,7 +6,7 @@
     <a class="nav-link @if (Route::is('home')) active @endif" aria-current="page" href="{{ route('home') }}">Home Comics</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link " href="#">Comix index</a>
+    <a class="nav-link @if (Route::is('comics.index')) active @endif" href="{{ route('comics.index') }}">Comics index</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Single Comic</a>
